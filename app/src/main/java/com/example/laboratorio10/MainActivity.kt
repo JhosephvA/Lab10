6ab4d3f7a78726b6d962189e7de881b89f84ffe7
JhosephvA <henry.ventura@tecsup.edu.pe>
2025-05-27 21:24:54 -0500

    Creamos ScreenInicio y modificamos el main

--- app/src/main/java/com/example/laboratorio10/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio10/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.laboratorio10.ui.theme.Laboratorio10Theme
+import com.example.laboratorio10.view.SeriesApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Laboratorio10Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SeriesApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Laboratorio10Theme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/laboratorio10/view/ScreenInicio.kt
@@ -0,0 +1,9 @@
+package com.example.laboratorio10.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+
+@Composable
+fun ScreenInicio() {
+    Text(text = "Hola")
+}
