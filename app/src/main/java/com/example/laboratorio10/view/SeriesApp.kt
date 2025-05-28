867976f7ae951c23a9c3dadf5fe528f1d35a6969
JhosephvA <henry.ventura@tecsup.edu.pe>
2025-05-27 21:57:20 -0500

    Modificados para ejecutar

--- app/src/main/java/com/example/laboratorio10/view/SeriesApp.kt
+++ app/src/main/java/com/example/laboratorio10/view/SeriesApp.kt
@@ -37,7 +37,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 @Composable
 fun SeriesApp() {
-    val urlBase = "http://10.0.2.2:8000/" // o tu IP si usarás un dispositivo externo
+    val urlBase = "http://192.168.1.3:8000/" // o tu IP si usarás un dispositivo externo
     val retrofit = Retrofit.Builder().baseUrl(urlBase)
         .addConverterFactory(GsonConverterFactory.create()).build()
     val servicio = retrofit.create(SerieApiService::class.java)
